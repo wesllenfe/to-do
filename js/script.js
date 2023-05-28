57e7a899c3ac024305b8a75917a68c3c39abcd7a
Wesllen Felipe Langaro Raiser da Cruz <wesllenf11@gmail.com>
2023-05-28 18:53:36 -0300

    Funcionalidade ao botão editar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+let oldInputValue;
+
 // Funções
 const saveTodo = (Text) =>{
 
@@ -37,6 +39,24 @@ const saveTodo = (Text) =>{
   todoInput.focus()
 }
 
+const toggleForms = () =>{
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+}
+
+const updateTodo = (text) =>{
+
+  const todos = document.querySelectorAll(".todo")
+
+  todos.forEach((todo)=>{
+    let todoTitle = todo.querySelector("h3")
+
+    if(todoTitle.innerText === oldInputValue){
+      todoTitle.innerHTML = text;
+    }
+  })
+}
 
 // Eventos
 
@@ -55,6 +75,11 @@ document.addEventListener("click", (e) => {
 
   const targetEl = e.target;
   const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if(parentEl && parentEl.querySelector("h3")){
+    todoTitle = parentEl.querySelector("h3").innerHTML;
+  }
 
   if (targetEl.classList.contains("finish-todo")) {
     parentEl.classList.toggle("done");
@@ -63,4 +88,29 @@ document.addEventListener("click", (e) => {
   if(targetEl.classList.contains("remove-todo")){
     parentEl.remove()
   }
+
+  if(targetEl.classList.contains("edit-todo")){
+    toggleForms()
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
+  }
 });
+
+cancelEditBtn.addEventListener("click", (e) =>{
+  e.preventDefault();
+
+  toggleForms();
+})
+
+editForm.addEventListener("submit", (e) =>{
+  e.preventDefault()
+
+  const editInputValue = editInput.value
+
+  if(editInputValue){
+    updateTodo(editInputValue)
+  }
+
+  toggleForms()
+})
